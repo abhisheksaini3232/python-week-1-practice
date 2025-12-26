92acc05d15524664fcf2c0fcb0bdc57dde45a05d
AbhishekSaini3232 <abhishekbadthunia3232@gmail.com>
2025-12-26 19:27:03 +0530

    Started new ATM project

---
+++ ATM simulation/atmSimulation.py
@@ -0,0 +1,11 @@
+# Project 2 — ATM Simulation (Console-Based)
+# Concepts Used:
+#  Loops, conditions, functions
+# Features
+# Check balance
+# Deposit money
+# Withdraw money
+# Exit system
+
+balance=0
+
