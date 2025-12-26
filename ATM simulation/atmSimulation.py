01fc6163bb31cb689b01a38c2644dd0861e15ae7
AbhishekSaini3232 <abhishekbadthunia3232@gmail.com>
2025-12-26 19:54:09 +0530

    FInal code

--- ATM simulation/atmSimulation.py
+++ ATM simulation/atmSimulation.py
@@ -7,30 +7,34 @@
 # Withdraw money
 # Exit system
 
+
 balance=0
 def depositMoney():
+      global balance
       amount=int(input("Enter the amount you want to add: "))
-      balance+=amount
+      balance=amount+balance
       print("The balance has been updated to ",balance)
 
 def withdrawMoney():
+      global balance
       amount=int(input("Enter the amount you want to add: "))
       if(amount<balance):
         balance=balance-amount
         print("The balance has been updated to ",balance)
+      else:
+         print("Low Balance")  
       
 while True:
- try:
-    option=int(input("Enter the option\n1.Check balance 2.Deposit money 3.Withdraw money 4.Exit system"))
+    option=int(input("Enter the option\n1.Check balance 2.Deposit money 3.Withdraw money 4.Exit system\n:"))
     if(option==1):
        print("Your balance is ",balance)
     elif(option==2):
        depositMoney()
     elif(option==3):
        withdrawMoney()
-    elif(option=4):
+    elif(option==4):
        break       
- except valueError:
-   print("Please enter appropriate value")
+    else:
+     print("Please enter appropriate value")
 
 
