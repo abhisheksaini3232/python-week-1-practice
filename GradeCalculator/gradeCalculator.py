da8260c3e0777dcd6986fa591a7b5c09e7943f09
AbhishekSaini3232 <abhishekbadthunia3232@gmail.com>
2025-12-29 14:13:12 +0530

    started making new project grade calculator

---
+++ GradeCalculator/gradeCalculator.py
@@ -0,0 +1,8 @@
+# Project 1 — Student Marks → Grade Calculator
+# Concepts Used:
+# Variables, conditions, functions
+# Features
+# Input marks for multiple subjects
+# Calculate percentage
+# Assign grade
+# Handle invalid input
