c6570c2153491c6fc881afcd6ab22df06d4a5bb1
AbhishekSaini3232 <abhishekbadthunia3232@gmail.com>
2025-12-29 20:07:11 +0530

    changes

--- Contact Book Application/contactBookApplication.py
+++ Contact Book Application/contactBookApplication.py
@@ -1,3 +1,4 @@
+# Project 3 — Contact Book Application
 # Concepts Used:
 #  Dictionaries, lists, functions
 # Features
@@ -6,4 +7,24 @@
 # Delete contact
 # Display all contacts
 
-phoneBook={}
+contactDict={}
+
+def addContact():
+  name=input("Enter the name: ")
+  while True:
+    try:
+      # number=input("Enter the contact number(separate with ','' if multiple): ").split(",")
+      numbers=list(map(int,input("Enter the contact number(separate with ','' if multiple): ").split(",")))
+      for i in range (len(numbers)):
+        length=len(str(numbers[i]))
+        if(length==10):
+          continue
+        
+    except ValueError: print("The contact number length is less than 10")      
+      
+        
+  contactDict["name"]=numbers
+  print(f"The contact for {name} added with number {numbers}")
+  print(contactDict)
+
+addContact()
