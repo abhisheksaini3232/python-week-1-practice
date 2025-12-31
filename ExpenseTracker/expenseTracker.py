ddaaae0e02f75aa92487ae5602c1c70665efa92e
AbhishekSaini3232 <abhishekbadthunia3232@gmail.com>
2025-12-31 12:48:36 +0530

    started with exoenseTracker project

---
+++ ExpenseTracker/expenseTracker.py
@@ -0,0 +1,10 @@
+# Project 5 — Expense Tracker (File-Based)
+# Concepts Used:
+#  File handling, loops, functions
+# Features
+# Add daily expense
+# Save to file
+# Read expense history
+# Show total monthly expense
+
+
